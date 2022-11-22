e85ef4a38cb882e469569fb104bf325bbd02bbb2
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2022-11-23 00:19:16 +0100

    improve build script for cleaner output

--- cmd/build.ps1
+++ cmd/build.ps1
@@ -1,1 +1,9 @@
 hugo --buildDrafts
+$htmlfiles = Get-ChildItem .\public\**\*.html
+foreach ($file in $htmlfiles)
+{
+(Get-Content $file.PSPath) |
+Foreach-Object { $_ -replace "^\s+$", "" } |
+Where-Object {$_.trim() -ne "" } |
+Set-Content $file.PSPath
+}
