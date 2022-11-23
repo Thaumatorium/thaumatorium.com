24dd7bd7bac835fe968592e8916a8c95a9e6c1b0
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2022-11-23 23:52:20 +0100

    polish build script

--- cmd/build.ps1
+++ cmd/build.ps1
@@ -1,9 +1,8 @@
 hugo --buildDrafts
 $htmlfiles = Get-ChildItem .\public\ -Filter *.html -Recurse
-foreach ($file in $htmlfiles)
-{
-(Get-Content $file.PSPath) |
-Foreach-Object { $_ -replace "^\s+$", "" } |
-Where-Object {$_.trim() -ne "" } |
-Set-Content $file.PSPath
+foreach ($file in $htmlfiles) {
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace "^\s+$", "" } |
+    Where-Object { $_.trim() -ne "" } |
+    Set-Content $file.PSPath
 }
